a614081d0bf55bbd690cb161e47d34166ed6ff66
AndreasBGJensen <bgandreas@hotmail.com>
2020-07-29 12:50:51 +0200

    trial change react test - changed back to the right

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -5,5 +5,5 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeDisabled();
+  expect(linkElement).toBeInTheDocument();
 });
