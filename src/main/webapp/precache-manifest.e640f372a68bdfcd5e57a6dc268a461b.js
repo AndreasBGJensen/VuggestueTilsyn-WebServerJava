0a32b8ca99b1ff34e5de26fbb3ce24363c7cbf28
AndreasBGJensen <bgandreas@hotmail.com>
2020-07-29 13:25:07 +0200

    Automated pipeline

---
+++ src/main/webapp/precache-manifest.e640f372a68bdfcd5e57a6dc268a461b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0f3d891a12e7872a89cf516c68f0e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4305e558f375d3736a84",
+    "url": "/static/css/main.7ca06e2b.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4305e558f375d3736a84",
+    "url": "/static/js/main.c367d311.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
